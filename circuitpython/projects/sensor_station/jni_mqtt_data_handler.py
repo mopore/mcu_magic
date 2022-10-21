c6f07d69ad62a097cfdecdea02735ea4a1e6d124
mopore <jni@mopore.org>
2022-10-21 22:30:44 +0200

    Use internal button to control refurbished LED signalization.

--- circuitpython/projects/sensor_station/jni_mqtt_data_handler.py
+++ circuitpython/projects/sensor_station/jni_mqtt_data_handler.py
@@ -1,6 +1,7 @@
 import time
 import board
 import neopixel
+from digitalio import DigitalInOut, Pull
 
 from jni_data_handler import DataHandler
 from jni_sensor_station import SensorData, SensorStation
@@ -9,10 +10,12 @@ from jni_wifi import connect_wifi, disconnect_wifi
 from jni_mqtt_bridge import MqttBridge
 
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-ORANGE = (255, 165, 0)
-BLACK = (0, 0, 0)
+LED_RED = (255, 0, 0)
+LED_GREEN = (0, 255, 0)
+LED_ORANGE = (255, 165, 0)
+LED_BLACK = (0, 0, 0)
+BUTTON_YES = False
+BUTTON_NO = True
 
 
 class ThisMotionEventReceiver(MotionEventReceiver):
@@ -33,63 +36,84 @@ class ThisMotionEventReceiver(MotionEventReceiver):
 class MqttDataHandler(DataHandler):
 
 	ALIVE_TICK_INTERVAL = 10
-	ALIVE_LED_MAX = 20
+	ALIVE_LED_MAX = 5
 
-	def __init__(self, bridge: MqttBridge, pixel: neopixel.NeoPixel) -> None:
+	def __init__(
+		self, bridge: MqttBridge, 
+		pixel: neopixel.NeoPixel, 
+		button: DigitalInOut
+	) -> None:
 		self.motionHandler = MotionHandler(ThisMotionEventReceiver(bridge))
 		self.bridge = bridge
 		self.last_tick = 0
 		self.alive_led_counter = 0
 		self.pixel = pixel
+		self.button = button
+		self.show_led = True
+		self.button_ready = True
 
 	def handle(self, sensor_data: SensorData) -> None:
+		# Publish data
 		self.motionHandler.handle_distance_data(sensor_data.distance)
 		self.bridge.publish_light_level(sensor_data.light_level)
-
 		if sensor_data.aq is not None:
 			self.bridge.publish_co2(sensor_data.aq.co2)
 			self.bridge.publish_humidity(sensor_data.aq.humidity)
 			self.bridge.publish_temperature(sensor_data.aq.temperature)
 
+		# Publish alive tick
 		now = time.monotonic()
 		time_after_tick = now - self.last_tick
 		if time_after_tick > self.ALIVE_TICK_INTERVAL:
 			self.bridge.publish_alive_tick()
 			self.last_tick = now
 
-		if self.bridge.keep_running is True:
-			self.alive_led_counter += 1
-			if self.alive_led_counter == (self.ALIVE_LED_MAX - 1):
-				self.pixel.fill(GREEN)
-			if self.ALIVE_LED_MAX == self.alive_led_counter:
-				self.alive_led_counter = 0
-				self.pixel.fill(BLACK)
+		# LED control via internal button
+		if self.button_ready:
+			if BUTTON_YES == self.button.value:
+				self.show_led = not self.show_led
+				self.button_ready = False
 		else:
-			self.pixel.fill(RED)
+			if BUTTON_NO == self.button.value:
+				self.button_ready = True
+
+		# LED signalization
+		if self.bridge.keep_running is True:
+			if self.show_led:
+				self.alive_led_counter += 1
+				if self.alive_led_counter == (self.ALIVE_LED_MAX - 1):
+					self.pixel.fill(LED_GREEN)
+				if self.ALIVE_LED_MAX == self.alive_led_counter:
+					self.alive_led_counter = 0
+					self.pixel.fill(LED_BLACK)
+		else:
+			self.pixel.fill(LED_RED)
 
 
 def prepare_datahandler() -> MqttDataHandler | None:
+	button = DigitalInOut(board.BUTTON)
+	button.switch_to_input(pull=Pull.UP)
 	pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)  # type: ignore
 	pixel.brightness = 0.03
 	try:
-		pixel.fill(ORANGE)
+		pixel.fill(LED_ORANGE)
 		connect_wifi()
 	except Exception as e:
 		print(f"Could not connect to Wifi: {e}")
-		pixel.fill(RED)
+		pixel.fill(LED_RED)
 		return
 
 	bridge: MqttBridge | None = None
 	try:
 		bridge = MqttBridge()
-		pixel.fill(GREEN)
+		pixel.fill(LED_GREEN)
 	except Exception as e:
 		print(f"Could not create bridge to MQTT broker: {e}")
-		pixel.fill(RED)
+		pixel.fill(LED_RED)
 		disconnect_wifi()
 	
 	if bridge is not None:
-		handler = MqttDataHandler(bridge, pixel)
+		handler = MqttDataHandler(bridge, pixel, button)
 		return handler
 	# Leave with None - will never happen at this point...
 
