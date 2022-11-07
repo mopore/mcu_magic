0508340d2f915960620a4c236fdd1de46b63521a
mopore <jni@mopore.org>
2022-11-07 20:56:41 +0100

    Refactoring.

--- circuitpython/playground/joy_featherwing/code.py
+++ circuitpython/playground/joy_featherwing/code.py
@@ -2,28 +2,7 @@
 # circup install adafruit_seesaw
 import time
 import board
-from micropython import const
 from adafruit_seesaw.seesaw import Seesaw
-
-i2c_bus = board.I2C()
-ss = Seesaw(i2c_bus)
-
-BUTTON_RIGHT = const(6)
-BUTTON_DOWN = const(7)
-BUTTON_LEFT = const(9)
-BUTTON_UP = const(10)
-BUTTON_SEL = const(14)
-button_mask = const(
-    (1 << BUTTON_RIGHT)
-    | (1 << BUTTON_DOWN)
-    | (1 << BUTTON_LEFT)
-    | (1 << BUTTON_UP)
-    | (1 << BUTTON_SEL)
-)
-STICK_X = const(3)
-STICK_Y = const(2)
-
-ss.pin_mode_bulk(button_mask, ss.INPUT_PULLUP)
 
 
 class Calibration:
@@ -36,6 +15,8 @@ class Calibration:
         self.x_max = start_x
         self.y_min = start_y
         self.y_max = start_y
+        self.x_rel = 0
+        self.y_rel = 0
 
     def update(self, x: int, y: int):
         if (abs(x - self.x) > 3) or (abs(y - self.y) > 3):
@@ -49,6 +30,8 @@ class Calibration:
                 self.y_max = y
             self.x = x
             self.y = y
+            self.x_rel = self._percentage_to_extreme(self.x, self.start_x, self.x_min, self.x_max)
+            self.y_rel = self._percentage_to_extreme(self.y, self.start_y, self.y_min, self.y_max) * -1
 
     def _percentage_to_extreme(self, actual: int, start: int, min: int, max: int) -> int:
         perc_extreme = 0
@@ -66,36 +49,82 @@ class Calibration:
         return int(perc_extreme) 
 
     def print(self):
-        x_rel = self._percentage_to_extreme(self.x, self.start_x, self.x_min, self.x_max)
-        y_rel = self._percentage_to_extreme(self.y, self.start_y, self.y_min, self.y_max) * -1
-        print(f"X: {x_rel}\t\tY: {y_rel}")
+        print(f"X: {self.x_rel}\t\tY: {self.y_rel}")
 
 
-print("Please move stick or press button.")
-start_x = ss.analog_read(STICK_X)
-start_y = ss.analog_read(STICK_Y)
-calib = Calibration(start_x, start_y)
+class JniJoyFw():
+    
+    def __init__(self) -> None:
+        i2c_bus = board.I2C()
+        ss = Seesaw(i2c_bus)
 
-while True:
-    x = ss.analog_read(STICK_X)
-    y = ss.analog_read(STICK_Y)
-    calib.update(x, y)
-    calib.print()
+        self.BUTTON_RIGHT = 6
+        self.BUTTON_DOWN = 7
+        self.BUTTON_LEFT = 9
+        self.BUTTON_UP = 10
+        self.BUTTON_SEL = 14
+        self.BUTTON_MASK = (
+            (1 << self.BUTTON_RIGHT) | 
+            (1 << self.BUTTON_DOWN) | 
+            (1 << self.BUTTON_LEFT) | 
+            (1 << self.BUTTON_UP) | 
+            (1 << self.BUTTON_SEL)
+        )
+        self.STICK_X = 3
+        self.STICK_Y = 2
+        ss.pin_mode_bulk(self.BUTTON_MASK, ss.INPUT_PULLUP)
+        self.ss = ss
+        start_x = ss.analog_read(self.STICK_X)
+        start_y = ss.analog_read(self.STICK_Y)
+        self.calib = Calibration(start_x, start_y)
+        self.button_down = False
+    
+    def tick(self) -> None:
+        x = self.ss.analog_read(self.STICK_X)
+        y = self.ss.analog_read(self.STICK_Y)
+        self.calib.update(x, y)
+        self.calib.print()
 
-    buttons = ss.digital_read_bulk(button_mask)
-    if not buttons & (1 << BUTTON_RIGHT):
-        print("Button A pressed")
+        buttons = self.ss.digital_read_bulk(self.BUTTON_MASK)
+        if not self.button_down:
+            if not buttons & (1 << self.BUTTON_RIGHT):
+                print("Button A pressed")
+                self.button_down = True
 
-    if not buttons & (1 << BUTTON_DOWN):
-        print("Button B pressed")
+            if not buttons & (1 << self.BUTTON_DOWN):
+                print("Button B pressed")
+                self.button_down = True
 
-    if not buttons & (1 << BUTTON_LEFT):
-        print("Button Y pressed")
+            if not buttons & (1 << self.BUTTON_LEFT):
+                print("Button Y pressed")
+                self.button_down = True
 
-    if not buttons & (1 << BUTTON_UP):
-        print("Button x pressed")
+            if not buttons & (1 << self.BUTTON_UP):
+                print("Button x pressed")
+                self.button_down = True
 
-    if not buttons & (1 << BUTTON_SEL):
-        print("Button SEL pressed")
+            if not buttons & (1 << self.BUTTON_SEL):
+                print("Button SEL pressed")
+                self.button_down = True
+        else:
+            if (
+                buttons & (1 << self.BUTTON_RIGHT) and 
+                buttons & (1 << self.BUTTON_DOWN) and 
+                buttons & (1 << self.BUTTON_LEFT) and 
+                buttons & (1 << self.BUTTON_UP) and 
+                buttons & (1 << self.BUTTON_SEL)
+            ):
+                self.button_down = False
 
-    time.sleep(0.1)
+
+def main() -> None:
+    wing = JniJoyFw()
+    print("Please move stick or press button.")
+
+    while True:
+        wing.tick()
+        time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    main()
