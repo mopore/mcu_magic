3416207b263a7a69ea322ddff5b7e38c6932e5de
mopore <jni@mopore.org>
2022-12-30 13:52:41 +0100

    Optimaztions

--- circuitpython/playground/uart_serial/receiver.py
+++ circuitpython/playground/uart_serial/receiver.py
@@ -7,7 +7,7 @@ def main() -> None:
 	print("Waiting for bytes on UART...")
 	uart = busio.UART(tx=board.TX, rx=board.RX, baudrate=19200)
 	while True:
-		data = uart.read(32)
+		data = uart.read(128)
 		if data:
 			data_string = "".join([chr(b) for b in data])
 			print(f"Received: {data_string}")
