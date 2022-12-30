005eef53d48cdd5ae46ff9af51c570d3d4f2a9f7
mopore <jni@mopore.org>
2022-12-30 21:49:00 +0100

    Optimizations

--- circuitpython/playground/uart_serial_rs485/receiver.py
+++ circuitpython/playground/uart_serial_rs485/receiver.py
@@ -2,14 +2,24 @@ import busio
 import board
 
 
+STOP_BYTE = 0x7E
+
+
 def main() -> None:
 	print("Waiting for bytes on UART...")
 	uart = busio.UART(tx=board.TX, rx=board.RX, baudrate=19200)
 	while True:
-		data = uart.read(128)
+		data = uart.read(32)
 		if data:
-			data_string = "".join([chr(b) for b in data])
-			print(f"Received: {data_string}", end="")
+			print()
+			chars = []
+			for b in data:
+				if b == STOP_BYTE:
+					print("[Received STOP_BYTE]", end="")
+				else:
+					print(b, end="")
+					chars.append(chr(b))	
+			print(f"Received: {chars}", end="")
 
 
 if __name__ == "__main__":
