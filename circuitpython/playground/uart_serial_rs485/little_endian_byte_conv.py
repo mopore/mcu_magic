3291ae29c80f59704278c5f594a1c7510f2673d7
mopore <jni@mopore.org>
2023-01-03 19:47:19 +0100

    Decribe purpose of this code.

--- circuitpython/playground/uart_serial_rs485/little_endian_byte_conv.py
+++ circuitpython/playground/uart_serial_rs485/little_endian_byte_conv.py
@@ -1,5 +1,7 @@
 def main() -> None:
-
+	# The following bytes come as a byte array in little endian format via the network
+	# Since the default on a Circuitpython MCU is big endian the conversion need to to
+	# respect the little endian source.
 	data_bytes = bytes([0x00, 0x0E, 0x27, 0x07])
 	# data_bytes = bytearray([0x00, 0x0E, 0x27, 0x07])  # Needs CircuitPython 8
 	# data_bytes = b'\x00\x0E\x27\x07'
