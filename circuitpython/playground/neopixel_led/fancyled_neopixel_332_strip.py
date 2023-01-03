6b6a89d85aeb441383e1a5234a6088e854718471
mopore <jni@mopore.org>
2023-01-03 22:37:30 +0100

    Add a gradient palette.

--- circuitpython/playground/neopixel_led/fancyled_neopixel_332_strip.py
+++ circuitpython/playground/neopixel_led/fancyled_neopixel_332_strip.py
@@ -10,19 +10,30 @@ import board
 import neopixel
 import adafruit_fancyled.adafruit_fancyled as fancy
 
-# Declare a 6-element RGB rainbow palette
-palette = [
-    fancy.CRGB(1.0, 0.0, 0.0),  # Red
-    0xFF660b,  # Orange
-    fancy.CRGB(0.5, 0.5, 0.0),  # Yellow
+# Declare a 5 element fire palette
+palette_all_equal = [
     0xFFFFFF,  # White
+    0xffe20b,  # Yellow
+    0xFF660b,  # Orange
+    0xa70f0d,  # Red
+    0x5b2200,  # Brown
 ]
+
+palette_weighted = [
+    (0, 0xFFFFFF),  # White
+    (0.05, 0xffe20b),  # Yellow
+    (0.2, 0xFF660b),  # Orange
+    (0.45, 0xa70f0d),  # Red
+    (0.7, 0x5b2200),  # Brown
+]
+
+palette = fancy.expand_gradient(palette_weighted, 100)
 
 
 def fire_led_strip() -> None:
     num_leds = 166
     # The strip is connected with pin14 of the UM FeatherS3
-    pixels = neopixel.NeoPixel(board.IO14, num_leds, brightness=0.25, auto_write=False)  # type: ignore
+    pixels = neopixel.NeoPixel(board.IO14, num_leds, brightness=0.1, auto_write=False)  # type: ignore
 
     offset = 0  # Positional offset into color palette to get it to 'spin'
 
