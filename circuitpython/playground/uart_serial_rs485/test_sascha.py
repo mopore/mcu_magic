412a9b07fd8594d850ea8de94546db20a8c3371e
mopore <jni@mopore.org>
2023-01-06 15:46:20 +0100

    better output

--- circuitpython/playground/uart_serial_rs485/test_sascha.py
+++ circuitpython/playground/uart_serial_rs485/test_sascha.py
@@ -15,36 +15,33 @@ ping_data_bytes = bytes([0x7E, 0xFF, 0x03, 0xEE, 0xFE, 0xFF, 0xFF, 0x00, 0x15, 0
 
 
 def transmit_ping() -> None:
-	print("Sending ping...")
-	uart.write(ping_data_bytes)
-	time.sleep(DELAY_SECONDS)
-	print("Ping sent.")
+    print("Sending ping...")
+    uart.write(ping_data_bytes)
+    time.sleep(DELAY_SECONDS)
+    print("Ping sent.")
 
 
 def wait_for_response() -> None:
-	print("Waiting for response...")
-	chars = []
-	while True:
-		data = uart.read(32)
-		if data:
-			print()
-			for b in data:
-				if b == STOP_BYTE:
-					if len(chars) > 0:
-						word = "".join(chars)
-						print(f"\n\nReceived: {word}\n")
-						chars = []
-				else:
-					print(hex(b), end=" ")
-					chars.append(chr(b))	
-
+    print("Waiting for response...")
+    chars = []
+    while True:
+        data = uart.read(32)
+        if data:
+            print()
+            for b in data:
+                print(hex(b), end=" ")
+                chars.append(chr(b))	
+            #  if len(chars) > 0:
+            #      word = "".join(chars)
+            #      print(f"\n\nReceived: {word}\n")
+            #      chars = []
 
 def main() -> None:
-	#  for _ in range(10):
-	#      transmit_ping()
+    #  for _ in range(10):
+    #      transmit_ping()
     #  
-	wait_for_response()
+    wait_for_response()
 
 
 if __name__ == "__main__":
-	main()
+    main()
