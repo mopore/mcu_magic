4a4e208d260b9fdbe524c232fb4d2daa25543703
mopore <jni@mopore.org>
2023-01-15 23:03:42 +0100

    Better documentation for pins of other boards.

--- circuitpython/playground/noods/pwm.py
+++ circuitpython/playground/noods/pwm.py
@@ -7,8 +7,11 @@ import pwmio
 
 def main() -> None:
 
-	# This uses the 3 adjacent SPI pins on QtPy RP2040, but any pins will do.
-	PINS = [board.IO5]  # type: ignore List of pins, one per nOOd
+	# 
+	PINS = [board.MOSI]  # type: ignore List of pins, one per nOOd
+
+	# # This uses the 3 adjacent SPI pins on QtPy RP2040, but any pins will do.
+	# PINS = [board.IO5]  # type: ignore List of pins, one per nOOd
 	GAMMA = 2.6  # For perceptually-linear brightness
 
 	# Convert pin number list to PWMOut object list
@@ -20,7 +23,9 @@ def main() -> None:
 			# If using red, green, blue nOOds, you'll get a cycle of hues.
 			phase = (time.monotonic() - 2 * i / len(PINS)) * math.pi
 			brightness = int((math.sin(phase) + 1.0) * 0.5 ** GAMMA * 65535 + 0.5)
+			print(brightness)
 			pin.duty_cycle = brightness
+			time.sleep(0.1)
 
 
 if __name__ == "__main__":
