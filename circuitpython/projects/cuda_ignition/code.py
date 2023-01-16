5ebea71ac8c9feded44e93a528594cb6d060f663
mopore <jni@mopore.org>
2023-01-16 19:50:41 +0100

    Provide control classes.

--- circuitpython/projects/cuda_ignition/code.py
+++ circuitpython/projects/cuda_ignition/code.py
@@ -3,19 +3,34 @@
 # circup install adafruit_datetime
 # circup install adafruit_minimqtt
 
-import jni_wifi
-import jni_mqtt_bridge
 import asyncio
 
+import jni_wifi
+import jni_mqtt_bridge
+import jni_relay_control
 
-def command_cb(message: str) -> None:
-    print(f"Received command: {message}")
 
+class CudaIgnition:
+    
+    def __init__(self) -> None:
+        self.relay_control = jni_relay_control.RelayControl()
+
+    def command_cb(self, message: str) -> None:
+        print(f"Received command: {message}")
+        if "ON" == message.upper().strip():
+            self.relay_control.turn_on()
+        elif "OFF" == message.upper().strip():
+            self.relay_control.turn_off()
+        else:
+            print(f"Unknown command: {message}")
+    
 
 async def main() -> None:
+    ignition = CudaIgnition()
+ 
     print("Hello, world!")
     jni_wifi.connect_wifi()
-    mqtt = jni_mqtt_bridge.MqttBridge("cudo_ignition", command_cb)
+    mqtt = jni_mqtt_bridge.MqttBridge("cudo_ignition", ignition.command_cb)
     mqtt_loop_task = asyncio.create_task(mqtt.loop())
     await asyncio.gather(mqtt_loop_task)
 
---
+++ circuitpython/projects/cuda_ignition/jni_relay_control.py
@@ -0,0 +1,40 @@
+import board
+import digitalio
+import time
+# If the relay is unset, the NC pin (Normally Connected) is mechanically 
+# connected to COM. NO (Normally Open) is mechanically disconnected.
+# When the relay is set, NC becomes disconnected from COM and NO is 
+# connected to COM
+
+
+class RelayControl:
+	def __init__(self) -> None:
+		# UM FeatherS3
+		self.relay_pin = digitalio.DigitalInOut(board.IO11)  # type: ignore    
+		self.relay_pin.direction = digitalio.Direction.OUTPUT
+
+	def turn_on(self) -> None:
+		self.relay_pin.value = True
+
+	def turn_off(self) -> None:
+		self.relay_pin.value = False
+
+
+def main() -> None:
+	control = RelayControl()
+
+	print("Turning relay on in 1 second")
+	time.sleep(1)
+	control.turn_on()
+	seconds_to_wait = 10
+	print(f"Waiting for {seconds_to_wait} seconds...")
+	time.sleep(seconds_to_wait)
+
+	print("Setting relay to off")
+	control.turn_off()
+
+	print("Done")
+
+
+if __name__ == "__main__":
+	main()
