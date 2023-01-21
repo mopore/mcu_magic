a92f7e80c1af0aca24eed610ddaa179cabb130de
mopore <jni@mopore.org>
2023-01-21 19:16:19 +0100

    Calibrate closed door

--- circuitpython/projects/wardrobe/code.py
+++ circuitpython/projects/wardrobe/code.py
@@ -1,40 +1,89 @@
 import time
-import math
-
 import board
-import pwmio
 
-import adafruit_vl53l1x
+import distance_provider
+import noods_enlighter
+import neopixel
+
+
+NOODS_ON = True
+NOODS_OFF = False
+
+
+class ThresholdProvider:
+
+	STATE_AWAITING_STABLE_VALUE = 0
+	STATE_CALIBRATED = 1
+
+	NO_MOVE_THRESHOLD_CM = 3
+	STABLE_FOR_CALIBRATION_SEC = 10
+
+	NP_GREEN = (0, 255, 0)
+	NP_RED = (255, 0, 0)
+	NP_BLACK = (0, 0, 0)
+
+	def __init__(self) -> None:
+		self.state = self.STATE_AWAITING_STABLE_VALUE
+		self.last_distance = -1
+		self.last_distance_time = 0
+		self.distance_threshold = -1
+		self.onboard_np = neopixel.NeoPixel(board.NEOPIXEL, 1)  # type: ignore
+		self.onboard_np.brightness = 0.03
+		self.onboard_np.fill(self.NP_GREEN)
+
+	def calibrate(self, distance: float) -> None:
+		now = time.monotonic()
+		if self.last_distance < 0:
+			self.last_distance = distance
+			self.last_distance_time = now
+			print("Waiting for stable value for door threshold...")
+		else:
+			if self.STATE_AWAITING_STABLE_VALUE == self.state:
+				diff = abs(self.last_distance - distance)
+				
+				if diff < self.NO_MOVE_THRESHOLD_CM:
+					self.onboard_np.fill(self.NP_GREEN)
+					time_passed = now - self.last_distance_time
+					if time_passed > self.STABLE_FOR_CALIBRATION_SEC:
+						self.distance_threshold = distance
+						self.state = self.STATE_CALIBRATED
+						self.onboard_np.fill(self.NP_BLACK)
+						self.onboard_np.brightness = 0
+						print(f"Threshold calibrated to {distance}")
+				else:
+					self.onboard_np.fill(self.NP_RED)
+					self.last_distance = distance
+					self.last_distance_time = now
+
+	def get_desired_state(self, distance: float) -> bool:
+		if distance - self.NO_MOVE_THRESHOLD_CM > self.distance_threshold:
+			return NOODS_ON
+		else:
+			return NOODS_OFF
 
 
 def main() -> None:
-
-	# Prepare VL53L1X
-	MODE_LONG = 2
-	i2c = board.STEMMA_I2C()  # type: ignore
-	vl53 = adafruit_vl53l1x.VL53L1X(i2c)
-	vl53.distance_mode = MODE_LONG
-	vl53.start_ranging()
-
-	# Prepare LED
-	PINS = [board.A2]  # type: ignore List of pins, one per nOOd
-	GAMMA = 2.6  # For perceptually-linear brightness
-	pin_list = [pwmio.PWMOut(pin, frequency=1000, duty_cycle=0) for pin in PINS]
+	print("Starting...")
+	dp = distance_provider.DistanceProvider()
+	pin = board.MOSI  # Adafruit QT Py ESP32-S3
+	enligher = noods_enlighter.NoodsEnlighter(pin)
+	led_state = NOODS_OFF
+	thresholdProvider = ThresholdProvider()
 
 	while True:                            # Repeat forever...
-		for i, pin in enumerate(pin_list):  # For each pin...
-			phase = (time.monotonic() - 2 * i / len(PINS)) * math.pi
-			brightness = int((math.sin(phase) + 1.0) * 0.5 ** GAMMA * 65535 + 0.5)
-			pin.duty_cycle = brightness
-			if vl53.data_ready:
-				distance = vl53.distance
-				if distance is not None:
-					try:
-						print(f"Distance: {distance:.0f} cm", end="\r")
-					except Exception as e:
-						print(e)
-				vl53.clear_interrupt()
-				time.sleep(.2)
+		distance = dp.read_distance()
+		if distance is not None:
+			if ThresholdProvider.STATE_CALIBRATED == thresholdProvider.state:
+				desired_state = thresholdProvider.get_desired_state(distance)
+				if desired_state != led_state:
+					led_state = desired_state
+					if led_state == NOODS_ON:
+						enligher.light_up()
+					else:
+						enligher.light_down()
+			else:
+				thresholdProvider.calibrate(distance)
+		time.sleep(.2)
 
 
 if __name__ == "__main__":
