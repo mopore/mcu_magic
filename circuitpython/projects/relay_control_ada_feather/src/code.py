8e5283eb02b5ef4013d85c020e35e183154a9ea7
mopore <jni@mopore.org>
2023-02-27 10:51:05 +0100

    Prodide generic 'cuda ignition' for Ada ESP32 Feather V2 boards.

---
+++ circuitpython/projects/relay_control_ada_feather/src/code.py
@@ -0,0 +1,51 @@
+# circup install asyncio adafruit_requests adafruit_datetime adafruit_minimqtt
+
+import asyncio
+
+import jni_wifi
+import jni_neo_mqtt_bridge
+import jni_relay_control
+
+try:
+	from secrets.jni_secrets import secrets
+except ImportError:
+	print("WiFi secrets are kept in secrets.py, please add them there!")
+	raise
+
+MQTT_SERVICE_NAME_KEY = "mqtt_service_name"
+
+
+class CudaIgnition:
+	
+	def __init__(self) -> None:
+		self.relay_control = jni_relay_control.RelayControl()
+
+	def command_cb(self, message: str, topic: str) -> None:
+		print(f"Received command: {message}")
+		if "ON" == message.upper().strip():
+			self.relay_control.turn_on()
+		elif "OFF" == message.upper().strip():
+			self.relay_control.turn_off()
+		else:
+			try:
+				seconds_to_wait = float(message)
+				self.relay_control.turn_on_for(seconds_to_wait)
+			except ValueError:
+				print(f"Unknown command: {message}")
+	
+
+async def main() -> None:
+	service_name = secrets[MQTT_SERVICE_NAME_KEY]
+	ignition = CudaIgnition()
+	subscriptions = [f"jniHome/services/{service_name}/command"]
+
+	print("Starting Cuda Ignition...")
+	jni_wifi.connect_wifi()
+	mqtt = jni_neo_mqtt_bridge.NeoMqttBridge(service_name, ignition.command_cb, subscriptions)
+	mqtt_loop_task = asyncio.create_task(mqtt.loop_async())
+	print("Setup complete.")
+	await asyncio.gather(mqtt_loop_task)
+
+
+if __name__ == "__main__":
+	asyncio.run(main())
---
+++ circuitpython/projects/relay_control_ada_feather/src/jni_relay_control.py
@@ -0,0 +1,47 @@
+import board
+import digitalio
+import time
+# The purpose of this project is to control a relay which cuts the power for the raspberry pi
+# with the hostname "ho.HomeNeoPi" in case there is a problem.
+# Consequently, the raspberry pi will be restarted when the relay functions with a cut-off.
+# The power cable for ho.HomePiNeo has to be conneted with the realy on COM  and NC 
+# (Normally Closed).
+
+
+class RelayControl:
+	def __init__(self) -> None:
+		# UM FeatherS3
+		# self.relay_pin = digitalio.DigitalInOut(board.IO11)  # type: ignore    
+		self.relay_pin = digitalio.DigitalInOut(board.D13)
+		self.relay_pin.direction = digitalio.Direction.OUTPUT
+
+	def turn_on(self) -> None:
+		self.relay_pin.value = True
+
+	def turn_off(self) -> None:
+		self.relay_pin.value = False
+
+	def turn_on_for(self, seconds: float) -> None:
+		self.turn_on()
+		time.sleep(seconds)
+		self.turn_off()
+
+
+def main() -> None:
+	control = RelayControl()
+
+	print("Turning relay on in 1 second")
+	time.sleep(1)
+	control.turn_on()
+	seconds_to_wait = 10
+	print(f"Waiting for {seconds_to_wait} seconds...")
+	time.sleep(seconds_to_wait)
+
+	print("Setting relay to off")
+	control.turn_off()
+
+	print("Done")
+
+
+if __name__ == "__main__":
+	main()
