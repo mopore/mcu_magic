3a9f56fedac8b8e6706107626df48903e64d0309
mopore <jni@mopore.org>
2023-04-07 11:08:30 +0200

    Provide an example for a standard servo with 180 degrees.

---
+++ circuitpython/playground/servos/standard_995R.py
@@ -0,0 +1,36 @@
+import time
+
+import board
+import pwmio
+
+from adafruit_motor import servo
+
+
+def control(engines: list[servo.Servo], angle: int) -> None:
+	for engine in engines:
+		engine.angle = angle
+
+
+def main() -> None:
+	pwms: list[pwmio.PWMOut] = [
+		pwmio.PWMOut(board.IO12, frequency=50),  # type: ignore
+	]
+
+	engines: list[servo.Servo] = []
+	for pwm in pwms:
+		engine = servo.Servo(pwm, min_pulse=600, max_pulse=2400)
+		engines.append(engine)
+
+	# angles = [0, 45, 90, 135, 180, 135, 90, 45, 0]
+	angles = [90]
+
+	print("Testing...")
+	
+	for angle in angles:
+		print(angle)
+		control(engines, angle)
+		time.sleep(2.0)
+
+
+if __name__ == "__main__":
+	main()
