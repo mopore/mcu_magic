d47f41a1219c8f57f030a15a46428a5b0102f304
mopore <jni@mopore.org>
2023-04-08 23:12:45 +0200

    Provide engine mangement.

--- circuitpython/projects/jni_proto_car/src/jni_engine_calibration.py
+++ circuitpython/projects/jni_proto_car/src/jni_engine_calibration.py
@@ -8,9 +8,9 @@ from adafruit_motor import servo
 
 class FrontEngine:
 
-	def __init__(self, pin_id, reverse: bool) -> None:
+	def __init__(self, pin_id, min_pulse: int, max_pulse: int, reverse: bool) -> None:
 		pwm = pwmio.PWMOut(pin_id, frequency=50)
-		self.c_servo = servo.ContinuousServo(pwm, min_pulse=750, max_pulse=2250)
+		self.c_servo = servo.ContinuousServo(pwm, min_pulse=min_pulse, max_pulse=max_pulse)
 		self.reverse = reverse
 
 	def control(self, input: float) -> None:
@@ -21,15 +21,18 @@ class FrontEngine:
 
 class SteeringEngine:
 	
-	def __init__(self, pin_id) -> None:
+	def __init__(self, pin_id, reverse: bool) -> None:
 		pwm = pwmio.PWMOut(pin_id, frequency=50)
 		# MG 995R servo can go from 0 to 180 degrees.
 		self.s_servo = servo.Servo(pwm, min_pulse=600, max_pulse=2400)
 		# Uncalibrated values: Center 90, Left 45 and Right 135
 		self.max_left = 49
 		self.max_right = 139
+		self.reverse = reverse
 	
 	def control(self, input: float) -> None:
+		if self.reverse:
+			input = -input
 		# Map input from -1 to 1 to max_left to max_right.
 		angle = int((input + 1) / 2 * (self.max_right - self.max_left) + self.max_left)
 		# print(f"Input: {input}, Angle: {angle}")
@@ -39,20 +42,28 @@ class SteeringEngine:
 class EngineCalibration:
 	
 	def __init__(self) -> None:
-		self.front_left = FrontEngine(board.IO14, reverse=False)  # type: ignore
-		self.front_right = FrontEngine(board.IO12, reverse=True)  # type: ignore
-		self.steering = SteeringEngine(board.IO6)  # type: ignore
+		self.front_left = FrontEngine(
+			board.IO14,  # type: ignore
+			min_pulse=750,
+			max_pulse=2250,
+			reverse=False)
+		self.front_right = FrontEngine(
+			board.IO12,  # type: ignore
+			min_pulse=760,
+			max_pulse=2260,
+			reverse=True)
+		self.steering = SteeringEngine(board.IO6, reverse=True)  # type: ignore
 
 
 def main() -> None:
 	sleep_time = 2
-	test_left_front = False
-	test_right_front = False
-	test_steering = True
+	test_front_left = True
+	test_front_right = True
+	test_steering = False
 
 	calibration = EngineCalibration()
 
-	if test_left_front:
+	if test_front_left:
 		print("Testing front left engine...")
 		calibration.front_left.control(1)
 		time.sleep(sleep_time)
@@ -63,7 +74,7 @@ def main() -> None:
 		calibration.front_left.control(0)
 		time.sleep(sleep_time)
 
-	if test_right_front:
+	if test_front_right:
 		print("Testing front right engine...")
 		calibration.front_right.control(1)
 		time.sleep(sleep_time)
@@ -73,6 +84,9 @@ def main() -> None:
 		time.sleep(sleep_time)
 		calibration.front_right.control(0)
 		time.sleep(sleep_time)
+		while True:
+			calibration.front_right.control(0)
+			time.sleep(sleep_time)
 
 	if test_steering:
 		print("Testing steering engine...")
--- circuitpython/projects/jni_proto_car/src/jni_engine_management.py
+++ circuitpython/projects/jni_proto_car/src/jni_engine_management.py
@@ -8,8 +8,8 @@ class EngineManagement:
 		self.calibration = jni_engine_calibration.EngineCalibration()
 		self.x_power = 0
 		self.y_power = 0
-		self.x_max_change_rate = REFRESH_RATE_HZ * 1
-		self.y_max_change_rate = REFRESH_RATE_HZ * 1
+		self.x_max_change_rate = (1 / REFRESH_RATE_HZ) * 1
+		self.y_max_change_rate = (1 / REFRESH_RATE_HZ) * 1
 	
 	def mitigate_throttle(self, y_demand: float) -> None:
 		demanded_change = y_demand - self.y_power
@@ -36,7 +36,6 @@ class EngineManagement:
 		self.mitigate_throttle(y_demand)
 		print(f"Powering engines with x={self.x_power}, y={self.y_power}")
 
-		# FIXME Uncomment these lines when ready
-		# self.calibration.front_left.control(y_power)
-		# self.calibration.front_right.control(y_power)
-		# self.calibration.steering.control(x_power)
+		self.calibration.front_left.control(self.y_power)
+		self.calibration.front_right.control(self.y_power)
+		self.calibration.steering.control(self.x_power)
