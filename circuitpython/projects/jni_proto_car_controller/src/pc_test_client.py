75b24f1bcf1093b4fce5240e337e3bb1f20570ff
mopore <jni@mopore.org>
2023-04-13 23:14:30 +0200

    Provide time-based testing from PC.

--- circuitpython/projects/jni_proto_car_controller/src/pc_test_client.py
+++ circuitpython/projects/jni_proto_car_controller/src/pc_test_client.py
@@ -1,12 +1,51 @@
 import socket
 import struct
+import asyncio
+import random
+import time
 
-# SERVER_IP = '192.168.199.245'
-SERVER_IP = '192.168.199.233'
+SERVER_IP = '192.168.199.245'
+# SERVER_IP = '192.168.199.233'
 SERVER_PORT = 8080
+# LOOP_TIME_BUDGET = 1  # 1 Hz
+LOOP_TIME_BUDGET = 0.1  # 10 Hz
+
+TIME_TO_LOOP = 5
 
 
-def main() -> None:
+def send_to_server(client_socket: socket.socket, x: int, y: int) -> None:
+	message = struct.pack('<bb', x, y)  # Little endian, two bytes
+	# print(f"Sending: {x}, {y}")
+	client_socket.send(message)
+
+
+async def loop_with_server(client_socket: socket.socket) -> None:
+	start_timestamp = time.monotonic()
+	keep_running = True
+	while keep_running:
+
+		loop_start_timestamp = time.monotonic()
+		if loop_start_timestamp - start_timestamp > TIME_TO_LOOP:
+			keep_running = False
+			print(f"Sending end command after {TIME_TO_LOOP} seconds")
+			send_to_server(client_socket, 99, 99)
+		else:
+			x = random.randint(-100, 100)
+			y = random.randint(-100, 100)
+			send_to_server(client_socket, x, y)
+
+		now = time.monotonic()
+		timepassed = now - loop_start_timestamp
+		time_left = LOOP_TIME_BUDGET - timepassed
+		ratio = timepassed / LOOP_TIME_BUDGET
+		print(f"Consumption ratio: {ratio:.2f}")
+		if ratio > 3:
+			raise Exception("System has a speed problem!")
+		if time_left > 0:
+			await asyncio.sleep(time_left)
+		
+
+async def main() -> None:
 	# Create a TCP/IP socket
 	print("Creating socket")
 	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,23 +55,10 @@ def main() -> None:
 	print(f"Connecting to {server_address}...")
 	client_socket.connect(server_address)
 
-	# Send data to the server
-	while True:
-		# Read two integers from the user
-		x = int(input('Enter the first integer: '))
-		y = int(input('Enter the second integer: '))
-
-		# Send the integers to the server
-		message = struct.pack('<bb', x, y)  # Little endian, two bytes
-
-		client_socket.send(message)
-		print(f"Sent {message}")
-		if x == 99 and y == 99:
-			break
-
+	await loop_with_server(client_socket)
 	# Close the socket
 	client_socket.close()
 
 
 if __name__ == "__main__":
-	main()
+	asyncio.run(main())
--- circuitpython/projects/jni_proto_car_controller/src/pc_test_server.py
+++ circuitpython/projects/jni_proto_car_controller/src/pc_test_server.py
@@ -2,9 +2,79 @@ import socket
 import struct
 import errno
 import time
+import asyncio
+
+# LOOP_TIME_BUDGET = 1  # 1 Hz
+LOOP_TIME_BUDGET = 0.1  # 10 Hz
 
 
-def main() -> None:
+def read_from_client(client_socket: socket.socket, b: bytearray) -> None | tuple[int, int]:
+	""" Return None if no data is available, otherwise return a tuple of two integers. """
+	try:
+		num_of_bytes = client_socket.recv_into(b)
+		if num_of_bytes > 0:
+			x, y = struct.unpack("<bb", b)  # Little endian, two bytes
+			return x, y
+	except OSError as err:
+		if errno.EAGAIN == err.args[0]:
+			return None
+		else:
+			client_socket.close()
+			print(f"Error with client sockert: {err}")
+			raise err
+
+
+def read_client_empty(client_socket: socket.socket, b: bytearray) -> None | tuple[int, int]:
+	response: None | tuple[int, int] = None
+	last_result = read_from_client(client_socket, b)	
+	if last_result is not None:
+		response = last_result
+
+	while last_result is not None:
+		result = read_from_client(client_socket, b)
+		if result is not None:
+			response = last_result
+		else:
+			last_result = None
+	
+	return response
+
+
+async def loop_with_client(client_socket: socket.socket, addr: socket.AddressInfo) -> None:
+	with client_socket:
+		client_socket.setblocking(False)
+		print('Connected with', addr)
+		keep_client = True
+
+		b = bytearray(2)
+		while keep_client:
+			start_timestamp = time.monotonic()
+			try:
+				result = read_client_empty(client_socket, b)
+				if result is not None:
+					x, y = result
+					# print(f"result is: {x}, {y}")
+					if x == 99 and y == 99:
+						print("Received end command!")
+						keep_client = False
+			except OSError as err:
+				print(f"Error with client sockert: {err}")
+				keep_client = False
+			if not keep_client:
+				print("Client connection end.")	
+				client_socket.close()
+			now = time.monotonic()
+			timepassed = now - start_timestamp
+			time_left = LOOP_TIME_BUDGET - timepassed
+			ratio = timepassed / LOOP_TIME_BUDGET
+			print(f"Consumption ratio: {ratio:.2f}")
+			if ratio > 3:
+				raise Exception("System has a speed problem!")
+			if time_left > 0:
+				await asyncio.sleep(time_left)
+
+
+async def main() -> None:
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	# Set the socket option to allow reuse of the address
@@ -18,40 +88,8 @@ def main() -> None:
 	while True:
 		print("Waiting for a connection...")
 		client_socket, addr = server_socket.accept()
-		with client_socket:
-			client_socket.setblocking(False)
-			print('Connected with', addr)
-			keep_client = True
-			last_timestamp = time.monotonic()
-			message_counter = 0
-
-			while keep_client:
-				now = time.monotonic()
-				timepassed = now - last_timestamp
-				try:
-					message = bytearray(2)
-					num_bytes = client_socket.recv_into(message)
-					if num_bytes > 0:
-						message_counter += 1
-						x, y = struct.unpack("<bb", message)  # Little endian, two bytes
-						if x == 99 and y == 99:
-							print("Received end command!")
-							keep_client = False
-							client_socket.close()
-							print("Client socket closed.")
-					if timepassed > 1:
-						last_timestamp = now	
-						print(f"{message_counter} messages/second")
-						message_counter = 0
-				except OSError as err:
-					if errno.EAGAIN == err.args[0]:
-						# No data available to read
-						time.sleep(.1)
-					else:
-						keep_client = False
-						client_socket.close()
-						print(f"Error with client sockert: {err}")
+		await loop_with_client(client_socket, addr)
 
 
 if __name__ == "__main__":
-	main()
+	asyncio.run(main())
